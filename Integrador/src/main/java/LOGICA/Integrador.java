c13c3d557350504a23620e6d690c784ace78fac5
andresore <andresore@gmail.com>
2025-09-16 11:24:00 -0600

    UpdatePK
    Actualización de archivos Pokémon

---
+++ Integrador/src/main/java/LOGICA/Integrador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package LOGICA;
+
+/**
+ *
+ * @author Bodos
+ */
+public class Integrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Integrador/src/main/java/LOGICA/Pokemon.java
@@ -0,0 +1,14 @@
+package LOGICA;
+
+public abstract class Pokemon {
+    protected int numPokedex;
+    protected String nombrePokemon;
+    protected double peso;
+    protected String sexo;
+    protected int temporada;
+    
+    protected abstract void atacarPlacaje() ;
+    protected abstract void atacarAraniazo() ;
+    protected abstract void atacarMordisco() ;
+    
+}
